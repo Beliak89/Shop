b9cda651cc389ff016eeb3616d7393e848abebff
Your Name <you@example.com>
2022-01-28 20:24:12 +0300

    Commit with ellipse and numbers

--- Button.js
+++ Button.js
@@ -1,4 +1,7 @@
+let i = 0
 function onClick() {
     const ellipse1 = document.querySelector('.ellipse');
-    ellipse1.style.display="block";
+        ellipse1.style.display="block";
+    const count1 = document.querySelector('.count');
+        count1.innerText = ++i;
 }
